d959c61086a1f3d98574f242f1ae97b118e4e8d4
Brokkoly <brendankrull@gmail.com>
2021-12-01 14:40:11 -0600

    first commit who dis

---
+++ src/day1/index.ts
@@ -0,0 +1,40 @@
+import { readInput } from '../helpers';
+
+
+// readInputFromURL("https://adventofcode.com/2021/day/1/input").then(input => {
+// 	const inputNums=input.map(str=>Number.parseInt(str));
+// 	const output1 = aocD1Q1(inputNums);
+// 	const output2 = aocD1Q1(aocD1Q2(inputNums));
+// 	console.log(output1);
+// 	console.log(output2);
+// });
+const input=readInput('../../input.txt');
+	const inputNums=input.map(str=>Number.parseInt(str));
+	const output1 = aocD1Q1(inputNums);
+	const output2 = aocD1Q1(aocD1Q2(inputNums));
+	console.log(output1);
+	console.log(output2);
+/**
+ * 
+ * @param input 
+ * @returns 
+ */
+function aocD1Q1(input: number[]): number {
+	console.log(input);
+	let numIncreases = 0;
+	for (let i = 1; i < input.length; i++) {
+		if (input[i] > input[i - 1]) {
+			numIncreases++;
+		}
+	}
+	return numIncreases;
+}
+
+function aocD1Q2(input: number[]): number[] {
+	const windowArr = new Array<number>(input.length - 2);
+	for (let i = 1; i < input.length - 1; i++) {
+		let sum = input[i - 1] + input[i] + input[i + 1];
+		windowArr[i - 1] = sum;
+	}
+	return windowArr;
+}
---
+++ src/helpers.ts
@@ -0,0 +1,23 @@
+import * as fs from 'fs';
+//import fetch from 'node-fetch';
+export function readInput(path: string): string[] {
+	let data: string[] = [];
+	try {
+		data = fs.readFileSync(path, 'utf-8').toString().replace(/\r\n/g, '\n').split('\n');
+		console.log(data);
+	}
+	catch (err) {
+		console.error(err);
+	}
+	return data;
+}
+
+// export async function readInputFromURL(path: string): Promise<string[]>{
+// 	return fetch(path)
+// 		.then(res => res.text)
+// 		.then(res => {
+// 			return res.toString().replace(/\r\n/g, '\n').split('\n');
+// 		});
+// }
+
+export {};
